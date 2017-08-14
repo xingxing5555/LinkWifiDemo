68a33fb5a96ecec8a433514a569c59b30c675423
zhaolewei <lewei.zhao@easyto.com>
2017-08-14 09:15:45 +0800

    集成JPush

---
+++ app/src/main/java/com/zlw/main/linkwifidemo/MyApp.java
@@ -0,0 +1,35 @@
+package com.zlw.main.linkwifidemo;
+
+import android.app.Application;
+
+import com.zlw.main.linkwifidemo.utils.Logger;
+import com.zlw.main.linkwifidemo.utils.blankj.PhoneUtils;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class MyApp extends Application {
+    private static final String TAG = MyApp.class.getSimpleName();
+    private static MyApp ins;
+
+    public static MyApp getInstance() {
+        return ins;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Logger.d(TAG, "=========>>MyApp<<========= ");
+        ins = this;
+        init();
+    }
+
+    void init() {
+        Logger.d(TAG, "初始化 JPush 中... ");
+
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+        String imei = PhoneUtils.getIMEI();
+        Logger.i(TAG, "本机imei、Jpush 别名:  " + imei);
+        JPushInterface.setAlias(getApplicationContext(), 1, "" + imei);
+    }
+}
